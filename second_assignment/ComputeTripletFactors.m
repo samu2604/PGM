6d1284aa1f4116f454aebba6fd4d6de3c58c4dcb
samu2604 <samuele.firmani89@gmail.com>
2020-02-01 20:35:22 +0100

    Implemented and tested ComputeTripletFactors.m

--- second_assignment/ComputeTripletFactors.m
+++ second_assignment/ComputeTripletFactors.m
@@ -29,4 +29,21 @@ factors = repmat(struct('var', [], 'card', [], 'val', []), n - 2, 1);
 
 % Your code here:
 
+for index = 1:(n-2)
+  factors(index).var(1) = index;
+  factors(index).var(2) = index + 1;
+  factors(index).var(3) = index + 2;
+  factors(index).card(1) = K;
+  factors(index).card(2) = K;
+  factors(index).card(3) = K;
+  factors(index).val(1:(K*K*K)) = 1;
+  for triplet_index = 1:length(tripletList) 
+    val_index = AssignmentToIndex(tripletList(triplet_index).chars, factors(index).card);
+    factors(index).val(val_index) = tripletList(triplet_index).factorVal;
+  end;
+  % normalization of the factor to have the 2000th most frequent triplet's factor equal to 1 
+  factors(index).val /= tripletList(end).factorVal;  
+end;
+
+
 end
