4cab53e7e34f3b3146f5e0b9a234f792e609ab0e
Samuele Firmani <samuele.firmani@fcagroup.com>
2021-06-03 12:20:13 +0200

    Implemented ComputeInitialPotentials.m for Exact-Inference-Assignment

--- Exact-Inference-Assignment/ComputeInitialPotentials.m
+++ Exact-Inference-Assignment/ComputeInitialPotentials.m
@@ -40,6 +40,33 @@ P.edges = zeros(N);
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+P.edges = C.edges;
+[Variables, map] = unique([C.factorList.var]);
+cards = [C.factorList.card];
+cards = cards(map);
+used_factors = zeros(size(C.factorList));
+
+for i = 1:N
+
+    P.cliqueList(i).var = C.nodes{i};
+    P.cliqueList(i).card = cards(C.nodes{i});
+
+    tmpFactor = struct('var', [], 'card', [], 'val', []);
+    tmpFactor.var = P.cliqueList(i).var;
+    tmpFactor.card = P.cliqueList(i).card;
+    tmpFactor.val = ones(1, prod(P.cliqueList(i).card));
+
+    for j = 1:length(C.factorList)
+        if used_factors(j) == 0 && all(ismember(C.factorList(j).var, C.nodes{i})) == 1   % I look for factors whose scope is included in the var list of the i-th node
+            tmpFactor = FactorProduct(tmpFactor, C.factorList(j));
+            used_factors(j) = 1;    
+        end
+    end
+
+    P.cliqueList(i).val = tmpFactor.val;
+
+end    
+
 
 end
 
